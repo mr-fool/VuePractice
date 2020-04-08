adcdee17681a81f810a36b64dfbb90caef90c7be
mr-fool <mr-fool@users.noreply.github.com>
2020-04-07 18:57:17 -0600

    fading effect

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 Vue.component("grocery-item", {
-    template: `<li>{{title}}<li>`,
-    props: ["title"]
+    template: `<li v-on:click="bought =!bought" v-bind:class="{bought: bought}">{{title}}</li>`,
+    props: ["title"],
+    data: function() {
+        return { 
+            bought: false
+        }
+    }
 });
 new Vue({
     el: "#app",
